eca53d06d245b45f97f5c8c221526c647655f88e
muripoLife <life.is.too.high.diffciulty@gmail.com>
2017-05-12 02:34:43 +0000

    ex04_関数化

--- NaturalLanguageProcessing/ex04.py
+++ NaturalLanguageProcessing/ex04.py
@@ -8,3 +8,18 @@ for i in range(len(charWordList)):
 	else:
 		charChoiceList.append(tmpCharList[0]+tmpCharList[1])
 print(charChoiceList)
+
+# 関数化
+# char = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+# charWordList = char.split(" ")
+# def n_gram(charList):
+# 	charChoiceList = []
+# 	for i in range(len(charWordList)):
+# 		tmpCharList = list(charWordList[i])
+# 		if i == 0 or i == 4 or i == 5 or i == 6 or i == 7 or i == 8 or i == 14 or i == 15 or i == 18:
+# 			charChoiceList.append(tmpCharList[0])
+# 		else:
+# 			charChoiceList.append(tmpCharList[0]+tmpCharList[1])
+# 	return charChoiceList
+# charChoiceList = n_gram(charWordList)
+# print(charChoiceList)
