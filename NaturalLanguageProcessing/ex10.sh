949adcc5f299868ebfc10cadc845ae6c4f9a77a8
muripoLife <life.is.too.high.diffciulty@gmail.com>
2017-05-16 02:18:15 +0000

    ex10

---
+++ NaturalLanguageProcessing/ex10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 読み込むファイル定義
+FILENAME="./file/hightemp.txt"
+# 配列定義
+declare -a array=()
+# ファイルの行数のcount
+row_count=0;
+# 一行づつ読み込む
+while read LINE;
+do
+	# 整数演算を行う
+	row_count=`expr $row_count + 1`
+	array+=($LINE)
+done < $FILENAME
+# 結果をコマンドラインに表示
+echo $row_count
