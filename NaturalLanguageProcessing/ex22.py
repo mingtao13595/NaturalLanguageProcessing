556d5b39da0111c9485f109a1b934978c4164a08
muripoLife <life.is.too.high.diffciulty@gmail.com>
2017-05-19 02:41:22 +0000

    ex22_リファクタリング

--- NaturalLanguageProcessing/ex22.py
+++ NaturalLanguageProcessing/ex22.py
@@ -6,8 +6,8 @@ import re
 
 # ファイルを使って開き、透過的なエンコード/デコードする
 json_file = codecs.open('./file/jawiki-country.json', 'r', 'utf_8')
-repatter_int = re.compile('Category:.+\]')
-# repatter_category_word = re.compile('Category:.+]')
+# 正規表現を書く
+repatter  = re.compile('Category:.+\]')
 
 if __name__ == "__main__":
 	articleList = []
@@ -17,12 +17,16 @@ if __name__ == "__main__":
 		articleList.append(fileLineList)
 
 	category_match = open('./file/ex22_practice.txt', 'w')
+
 	for line in articleList:
 		for raw in line:
-			category_row = repatter_int.findall(raw)
+			category_row = repatter.findall(raw)
 			if len(category_row) != 0:
-				category_word = re.sub(r']].+', '', category_row[0])
-				category_word = re.sub(']]', '', category_word)
+				category_word = category_row[0]
+				# 扶養文字列削除
+				category_word = re.sub(r']].+', '', category_word)
+				category_word = re.sub(']]'   , '', category_word)
 				category_word = re.sub(r'\|.+', '', category_word)
-				category_match.write(str(category_word)+"\n")
+				category_match.write(category_word + "\n")
+				print(category_word)
 	category_match.close()
