a24e4b5175267016a052365e0574731941e4850d
muripoLife <life.is.too.high.diffciulty@gmail.com>
2017-05-24 07:49:24 +0000

    ex37途中

---
+++ NaturalLanguageProcessing/ex37.py
@@ -0,0 +1,51 @@
+# coding: UTF-8
+#!/usr/bin/env python
+
+import MeCab
+import re
+import json
+
+def gets_surface(list):
+	surface_list = []
+	for line in list:
+		surface_list.extend([str(line.get("surface"))])
+	surface_list = sorted(surface_list)
+	return surface_list
+
+def deplicate_delete(list):
+	frequency_list = {}
+	deplicate_delete_list = set(list)
+	for word in deplicate_delete_list:
+		frequency_list[word] = 0
+	return frequency_list
+
+def gets_frequency(item_list, surface_list):
+	for word in surface_list:
+		item_list[word] += 1
+
+	frequency_list = {}
+	for k, v in sorted(item_list.items(), key=lambda x: -x[1]):
+		frequency_list[k] = v
+
+	return frequency_list
+
+def top_ten(list):
+	top_ten_list = {}
+	for k, v in list.items():
+		if len(top_ten_list) == 10:
+			return top_ten_list
+		if (k != '*')&(k != '。')&(k != '、'):
+			top_ten_list[k] = v
+
+try:
+	# ローカルJSONファイルの読み込み
+	with open('./file/ex30_practice.json', 'r') as f:
+		data = json.load(f)
+		surface_list         = gets_surface(data)
+		emply_frequency_list = deplicate_delete(surface_list)
+		frequency_list       = gets_frequency(emply_frequency_list, surface_list)
+		top_ten_list         = top_ten(frequency_list)
+
+		print(top_ten_list)
+except json.JSONDecodeError as e:
+	print('JSONDecodeError: ', e)
