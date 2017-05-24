c41ac1d2686183c7988edb9ba3417e365c314c1b
muripoLife <life.is.too.high.diffciulty@gmail.com>
2017-05-24 04:14:54 +0000

    ex33

---
+++ NaturalLanguageProcessing/ex33.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+#!/usr/bin/env python
+
+import MeCab
+import re
+import json
+
+def gets_verb(list):
+	verbList = []
+	for line in list:
+		if line.get("pos") == "サ変接続":
+			verbList.append(line)
+			# print(line)
+	return verbList
+
+try:
+	# ローカルJSONファイルの読み込み
+	with open('./file/ex30_practice.json', 'r') as f:
+		data = json.load(f)
+		verbList = gets_verb(data)
+		print(verbList)
+except json.JSONDecodeError as e:
+	print('JSONDecodeError: ', e)
