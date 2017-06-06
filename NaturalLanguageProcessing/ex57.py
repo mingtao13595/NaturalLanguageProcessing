81139855621baff3532a9d6ac1f9ba528980e643
muripoLife <life.is.too.high.difficulty@gmail.com>
2017-06-06 04:29:06 +0000

    ex57

--- NaturalLanguageProcessing/ex57.py
+++ NaturalLanguageProcessing/ex57.py
@@ -7,7 +7,6 @@ import pydot_ng as pydot
 import xml.etree.ElementTree as ET
 
 fname_parsed = 'nlp.txt.xml'
-
 if not os.path.exists(fname_parsed):
 	subprocess.run(
 		'java -cp "/root/stanford-corenlp-full-2016-10-31/*"'
@@ -25,17 +24,14 @@ root = ET.parse(fname_parsed)
 # sentence列挙、1文ずつ処理
 for sentence in root.iterfind('./document/sentences/sentence'):
 	sent_id = int(sentence.get('id'))
-
-	edges = []
+	edges   = []
 
 	# dependencies列挙
 	for dep in sentence.iterfind(
 		'./dependencies[@type="collapsed-dependencies"]/dep'
 	):
-
 		# 句読点はスキップ
 		if dep.get('type') != 'punct':
-
 			# governor、dependent取得、edgesに追加
 			govr = dep.find('./governor')
 			dept = dep.find('./dependent')
@@ -47,16 +43,13 @@ for sentence in root.iterfind('./document/sentences/sentence'):
 	if len(edges) > 0:
 		graph = pydot.Dot(graph_type='digraph')
 		for edge in edges:
-
-			id1 = str(edge[0][0])
+			id1    = str(edge[0][0])
 			label1 = str(edge[0][1])
-			id2 = str(edge[1][0])
+			id2    = str(edge[1][0])
 			label2 = str(edge[1][1])
-
 			# ノード追加
 			graph.add_node(pydot.Node(id1, label=label1))
 			graph.add_node(pydot.Node(id2, label=label2))
-
 			# エッジ追加
 			graph.add_edge(pydot.Edge(id1, id2))
 		graph.write_png('./file/{}.png'.format(sent_id))
