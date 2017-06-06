bd0e7323066e598614160565f59829ee3a4f88ee
muripoLife <life.is.too.high.difficulty@gmail.com>
2017-06-06 02:46:28 +0000

    ex56

--- NaturalLanguageProcessing/ex57.py
+++ NaturalLanguageProcessing/ex57.py
@@ -16,16 +16,11 @@ if not os.path.exists(fname_parsed):
 		shell=True,
 		check=True
 	)
-
-# xmlへパース
 root = ET.parse(fname_parsed)
 
-# 単語リストを生成
 rep_dict = {}
 for coreference in root.iterfind('./document/coreference/coreference'):
-	# 代表参照の取得
 	rep_text = coreference.findtext('./mention[@representative="true"]/text')
-	# 辞書生成
 	for mention in coreference.iterfind('./mention'):
 		if(mention.get('representative', 'false') == 'false'):
 			sent_id = int(mention.findtext('sentence'))
@@ -34,28 +29,19 @@ for coreference in root.iterfind('./document/coreference/coreference'):
 			if not (sent_id, start) in rep_dict:
 				rep_dict[(sent_id, start)] = (end, rep_text)
 
-# 本文の置換
 for sentence in root.iterfind('./document/sentences/sentence'):
 	sent_id  = int(sentence.get('id'))
 	org_rest = 0
-
-	# token列挙
 	for token in sentence.iterfind('./tokens/token'):
 		token_id = int(token.get('id'))
-
-		# 置換対象を列挙
 		if org_rest == 0 and (sent_id, token_id) in rep_dict:
 			(end, rep_text) = rep_dict[(sent_id, token_id)]
 			print('[' + rep_text + '] (', end='')
 			org_rest = end - token_id
 		print(token.findtext('word'), end='')
-
-		# 置換の終わりの判定
 		if(org_rest > 0):
 			org_rest -= 1
 			if(org_rest == 0):
 				print(')', end='')
 		print(' ', end='')
-
-	# 改行
 	print()
